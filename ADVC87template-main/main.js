d9f444d5c339c0fc35c4d2a9a89b5d1efa34c970
Ming-And-John <90568312+Ming-And-John@users.noreply.github.com>
2022-02-06 12:52:01 -0500

    Add files via upload

---
+++ ADVC87template-main/main.js
@@ -0,0 +1,56 @@
+
+var canvas=new fabric.Canvas("myCanvas");
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set()
+        
+}
+
+,window.addEventListener("keydown", my_keydown),
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == 82) // add appropriate keycode
+	{
+		get_image("rr.png")
+		
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		get_image("gr.png")
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		get_image("yr.png")
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		get_image("pr.png")
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	get_image("br.png")
+	}
+	
+}
+,)}
